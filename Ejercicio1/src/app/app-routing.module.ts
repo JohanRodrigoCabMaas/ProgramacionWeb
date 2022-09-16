59e6bb481d4a62be58ff9d96ba82d93594817a78
Johan Rodrigo Cab Maas <80074591+JohnRCabMaas@users.noreply.github.com>
2022-09-15 19:32:37 -0500

    Practica2

--- Ejercicio1/src/app/app-routing.module.ts
+++ Ejercicio1/src/app/app-routing.module.ts
@@ -1,7 +1,12 @@
-import { NgModule } from '@angular/core';
+import { Component, NgModule } from '@angular/core';
 import { RouterModule, Routes } from '@angular/router';
+import { HomeComponent } from './home/home.component';
+import { Seccion2Component } from './seccion2/seccion2.component';
 
-const routes: Routes = [];
+const routes: Routes = [
+  {path:'home', component:HomeComponent},
+  {path:'seccion2',component:Seccion2Component}
+];
 
 @NgModule({
   imports: [RouterModule.forRoot(routes)],
--- Ejercicio1/src/app/app.module.ts
+++ Ejercicio1/src/app/app.module.ts
@@ -3,10 +3,22 @@ import { BrowserModule } from '@angular/platform-browser';
 
 import { AppRoutingModule } from './app-routing.module';
 import { AppComponent } from './app.component';
+import { HeaderComponent } from './seccion/header/header.component';
+import { TextoComponent } from './seccion1/texto/texto.component';
+import { ContenidoComponent } from './seccion1/contenido/contenido.component';
+import { HomeComponent } from './home/home.component';
+import { NarbarComponent } from './seccion1/narbar/narbar.component';
+import { Seccion2Component } from './seccion2/seccion2.component';
 
 @NgModule({
   declarations: [
-    AppComponent
+    AppComponent,
+    HeaderComponent,
+    TextoComponent,
+    ContenidoComponent,
+    HomeComponent,
+    NarbarComponent,
+    Seccion2Component
   ],
   imports: [
     BrowserModule,
